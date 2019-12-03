297e4bf2933146086efcfbc9a2631760b69c21c9
Hamidreza Ramzani <hrra13801396h@gmail.com>
2019-12-03 17:52:39 +0330

    use promise instead callback functions

--- js/main.js
+++ js/main.js
@@ -7,7 +7,15 @@ $('#btnSearch').click(function () {
     var CityName = $('#SearchInput').val();
     if ($.trim(CityName) != "") {
         $('#loading').slideDown(0);
-        SetWeather(CityName);
+        SetWeather(CityName).then((response) => {
+            SetStatus(response);
+            $('#Main').fadeIn(150);
+            $('#loading').slideUp(0); 
+        }).catch((err) => {
+            alert("city not found!");
+            $('#Main').fadeIn(150);
+            console.log('has an error' , err);
+        });
     }
     else
         alert("Can not be empty");
@@ -18,21 +26,19 @@ $('#btnSearch').click(function () {
 var SetWeather = (CityName = "Tehran") => {
     var url = "https://api.openweathermap.org/data/2.5/weather?q=" + CityName + "&appid=7c5d387d3fdeefc4c073410663110b7f";
     $('#Main').slideUp(0);
-    $.ajax({
-        method: 'GET',
-        url: url,
-        success: function (msg) {
-            SetStatus(msg);
-            $('#Main').fadeIn(150);
-            $('#loading').slideUp(0);
-
-        },
-        error: function (msg) {
-            alert("city not found!");
-            $('#Main').fadeIn(150);
-
-        }
-    })
+    return new Promise((resolve , reject) => {        
+        $.ajax({
+                method: 'GET',
+                url: url,
+                success: function (response) {
+                    resolve(response);                    
+                },
+                error: function (error) {
+                    reject(error)                    
+                }
+        })
+    });
+    
 }
 
 var SetStatus = function (response) {
